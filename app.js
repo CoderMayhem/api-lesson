babcd7aff1f37bfc20328ce920da731fe10ead35
CoderMayhem <namanmishra1900@gmail.com>
2021-02-10 21:05:19 +0530

    feat: links js file to html code

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const https = require("https");  //https is a native node module therefore is not required to be downloaded separately.
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", function(req,res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Varanasi&appid=8131a14698a05a9d615459f8b73798bf&units=metric"
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  const query = req.body.cityName;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=8131a14698a05a9d615459f8b73798bf&units=metric"
   https.get(url, function(response){
     console.log(response.statusCode);
 
@@ -14,12 +20,12 @@ app.get("/", function(req,res){
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
       res.write("<p>The weather is currently " + description + "<p>");
-      res.write("<h1>The temperature in Banaras is " + temp + " deg Celcius.<h1>");
+      res.write("<h1>The temperature in "+query+" is " + temp + " deg Celcius.<h1>");
       res.send();
     });
   });
-});
 
+});
 
 
 app.listen(3000, function(){
